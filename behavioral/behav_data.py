cbfa1ec9e342cdee74a68809c392bfce678da2ba
fliem <franz.liem>
2015-09-28 15:09:27 +0200

    dropped bad subjects

--- behavioral/behav_data.py
+++ behavioral/behav_data.py
@@ -1,35 +1,44 @@
-#FIXME
-#check for duplicates
-import os, glob
+# FIXME
+# check for duplicates
+import os, glob, json
 import pandas as pd
 from utils import read_nki_cvs, leica_id_to_a_number_mapping, merge_dataframes, get_health_status
+import numpy as np
 
+save_file = '/Users/franzliem/Dropbox/LeiCA/sample/20150925_leicanki_sample.pkl'
 
+behav_files_path = '/Users/franzliem/Dropbox/LeiCA/assessment_data_20150817_entireNKI_wlongChlid_wnfb/'
+behav_files_stubs_list = ['Demos_', '_Age_']  # , 'STAI']
 
-behav_files_path = '/Users/franzliem/Dropbox/LeiCA/assessment_data_20150817/'
-behav_files_stubs_list =  ['Age_'] #['Demos_', '_Age_'] #, 'STAI']
-
-#preare subject name mapping
+# preare subject name mapping
 leica_subjects_file = '/Users/franzliem/Dropbox/Workspace/LeiCA/subjects/subjects_2015-09-06_r1-7.txt'
 nki_id_mapping_file = '/Users/franzliem/Dropbox/LeiCA/nki_r1_r6_matched_ids_unq.csv'
 
-leica_subjects = leica_id_to_a_number_mapping(leica_subjects_file, nki_id_mapping_file)
+# # get sex labels
+# variables_dict_file = '/Users/franzliem/Dropbox/Workspace/LeiCA/subjects/codebook/variable_dict.json'
+# with open(variables_dict_file) as fi:
+#     variables_dict = json.load(fi)
+# variables_dict['DEM_002']['response']
 
+# get M to A number mapping
+leica_subjects = leica_id_to_a_number_mapping(leica_subjects_file, nki_id_mapping_file)
 
 df = pd.DataFrame([])
 
 for file_stub in behav_files_stubs_list:
     file_list = glob.glob(os.path.join(behav_files_path, '*' + file_stub + '*'))
-    #import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
 
     for f in file_list:
         print(f)
         df_add = read_nki_cvs(f)
-        # if 'Subject Type' in df_add.columns:
-        #     print('Dropping Subject Type from df as this often causes duplicates.')
-        #     df_add = df_add.drop('Subject Type', axis=1)
-
         df = merge_dataframes(df, df_add)
+
+
+# sex mapping
+sex_mapping = {1: 'M', 2: 'F'}
+df['sex'] = df.DEM_002
+df = df.replace({'sex': sex_mapping})
 
 
 # get health status
@@ -37,15 +46,32 @@ diagnostics_file = glob.glob(os.path.join(behav_files_path, '*_Diagnostic Summar
 df_health = get_health_status(diagnostics_file)
 df = merge_dataframes(df, df_health)
 
-#add subjects id mapping to df
+# add subjects id mapping to df
 df = merge_dataframes(df, leica_subjects)
 
-#remove subjects that are not in leica
+# remove subjects that are not in leica
 df = df[~df.leica_id.isnull()]
 
-#remove exactly duplicated lines
+# remove exactly duplicated lines
 df = df[~df.duplicated()]
 
-#print ambigous duplicates (same index but different values
+# print ambigous duplicates (same index but different values
 print('DUPLICATE INDICES!!!')
 print(df.index.get_duplicates())
+print('')
+
+import warnings
+
+warnings.warn('NONAMBIGOUS INDEX INFO IN DF. FIX.')
+df = df[~df.index.duplicated()]
+
+
+# remove bad subjects (e.g. failed preprocessing/only few TRs)
+
+bad_subjects_list = ['0124028',
+                     'A00051882']
+
+for b in bad_subjects_list:
+    df = df[~(df.leica_id == b)]
+
+pd.to_pickle(df, save_file)
--- behavioral/utils.py
+++ behavioral/utils.py
@@ -1,17 +1,37 @@
 import os
 import pandas as pd
+import numpy as np
 
 
 def get_health_status(filename):
-    def get_no_axis_1(s):
-        if s.startswith('#CODE:V71.09'):
-            return True
-        else:
-            return False
+    #     def get_no_axis_1(s):
+    #         if s.startswith('#CODE:V71.09'):
+    #             return True
+    #         else:
+    #             return False
+    # #CODE:314  #DESC:Attention-Deficit/Hyperactivi
+    #         #CODE:296. Depression
+
+    def get_diagnosis(diag_str):
+        code_diag_mapping = {'#CODE:V71.09': 'no_axis_1',
+                             '#CODE:  #DESC:No Diagnosis or Condition on Axis I': 'no_axis_1',
+                             '#CODE:296.': 'depression',
+                             '#CODE:314': 'adhd',
+                             '#CODE:303': 'drugs',
+                             '#CODE:304': 'drugs',
+                             '#CODE:305': 'drugs'
+                             }
+        diag = 'other'
+        for k, v in code_diag_mapping.items():
+            if diag_str.startswith(k):
+                diag = v
+        return diag
 
     df = read_nki_cvs(filename)
-    df['no_axis_1'] = df['DIAG_01'].map(get_no_axis_1)
-    df = df[['no_axis_1', 'DIAG_01']]
+    df['diagnosis'] = df['DIAG_01'].map(get_diagnosis)
+    df['no_axis_1'] = df['diagnosis'] == 'no_axis_1'
+    df = df[['diagnosis', 'no_axis_1', 'DIAG_01']]
+
     return df
 
 
@@ -29,7 +49,6 @@ def read_nki_cvs(filename):
     return df
 
 
-
 def merge_dataframes(df1, df2):
     '''
     returns merged data frame
@@ -39,28 +58,35 @@ def merge_dataframes(df1, df2):
     return pd.merge(df1, df2, left_index=True, right_index=True, how='outer')
 
 
-
 def leica_id_to_a_number_mapping(leica_subjects_file, nki_id_mapping_file):
     '''
     for release 1...5 nki subjects '010...' finds 'A...' number
     for > r5 subjects
     returns dataframe with index is leica_subject_id and a_number
     '''
+    import numpy as np
 
     leica_subjects = pd.read_csv(leica_subjects_file, dtype=object, header=None, names=['leica_id'])
     leica_subjects.set_index(leica_subjects.leica_id, inplace=True)
 
     nki_id_mapping = pd.read_csv(nki_id_mapping_file)
     nki_id_mapping.set_index(nki_id_mapping.a_number, inplace=True)
-    nki_id_mapping['zero_number'] = ['01'+ v[4:] for v in  nki_id_mapping.m_number.values]
-
+    nki_id_mapping['zero_number'] = ['01' + v[4:] for v in nki_id_mapping.m_number.values]
+    bad = []
     for l in leica_subjects.index:
-        if l.startswith('A'): #use A number a
-            leica_subjects.loc[l,'a_number'] = l
+        if l.startswith('A'):  # use A number a
+            leica_subjects.loc[l, 'a_number'] = l
         else:
-            leica_subjects.loc[l,'a_number'] = \
-                nki_id_mapping.loc[nki_id_mapping.zero_number=='0161348', 'a_number'].index[0]
-
+            if np.any(nki_id_mapping.loc[nki_id_mapping.zero_number == l].index):
+                leica_subjects.loc[l, 'a_number'] = \
+                    nki_id_mapping.loc[nki_id_mapping.zero_number == l].index[0]
+            else:
+                bad.append(l)
     leica_subjects.set_index(leica_subjects.a_number, inplace=True)
+
+    import warnings
+    warnings.warn('REMOVING SUBJECTS WITHOUT M TO A MAPPING. FIX.')
+    leica_subjects = leica_subjects[~leica_subjects['a_number'].isnull()]
+
     leica_subjects.drop('a_number', axis=1, inplace=True)
     return leica_subjects
