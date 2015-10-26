4a6ecac5350c6f3ef899afb48056c79d8fccf610
fliem <franz.liem>
2015-10-26 17:19:02 +0100

    fixed and added check

--- backup/x_02_backup_rerun_all_for_checks.sh
+++ backup/x_02_backup_rerun_all_for_checks.sh
@@ -12,42 +12,15 @@ cd $source_path
 
 
 for D in 0*; do
-    echo "*************************************************"
-    echo $D
-    echo "*************************************************"
-
-    if [ ! -d $target_path/$D ]; then
-        echo "create new folder on /a ${D}"
-        cmd="newvp2 -p noid024_leica-nki -o ${D}"
-        echo $cmd
-        ${cmd}
-    fi
-
     cmd="rsync -av --no-o --no-g ${source_path}/${D}/ ${target_path}/${D}/"
-    echo $cmd
     $cmd
 done
 
 
-
-
 # because /a cannot take IDs with more than 9 chars, the heading 0 (after the A) is cut
 for D in A*; do
-    echo "*************************************************"
-    echo $D
-    echo "*************************************************"
-
     short_id=A${D:2}
-
-    if [ ! -d $target_path/$short_id ]; then
-        echo "create new folder on /a ${D} -> ${short_id}"
-        cmd="newvp2 -p noid024_leica-nki -o $short_id"
-        echo $cmd
-        $cmd
-    fi
-
     cmd="rsync -av --no-o --no-g ${source_path}/${D}/ ${target_path}/${short_id}/"
-    echo $cmd
     $cmd
 done
 
