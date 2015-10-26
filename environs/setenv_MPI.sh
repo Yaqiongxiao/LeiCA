06f2fa1827f5a508566c86ddc29b61f67ef252c2
fliem <franz.liem>
2015-10-26 10:09:20 +0100

    updated environment scripts for nipype 11

--- environs/setenv_MPI.sh
+++ environs/setenv_MPI.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-PYTHONPATH=${PYTHONPATH}:/home/raid2/liem/Dropbox/Workspace/LeiCA:/home/raid2/liem/Dropbox/Workspace/LeiCA/preprocessing:/home/raid2/liem/Dropbox/Workspace/LeiCA/preprocessing/cpac_0391_local
+PYTHONPATH=${PYTHONPATH}:/home/raid2/liem/Dropbox/Workspace:/home/raid2/liem/Dropbox/Workspace/LeiCA:/home/raid2/liem/Dropbox/Workspace/LeiCA/preprocessing:/home/raid2/liem/Dropbox/Workspace/LeiCA/preprocessing/cpac_0391_local
 export PYTHONPATH
 
 
 #make sure that DCMSTACK is called AFTER CPAC
-FSL --version 5.0 FREESURFER --version 5.3.0 AFNI CPAC DCMSTACK PANDAS NILEARN MATPLOTLIB NUMPY SEABORN
+FSL --version 5.0 FREESURFER --version 5.3.0 AFNI CPAC DCMSTACK PANDAS NILEARN SKLEARN MATPLOTLIB NUMPY SEABORN
--- environs/setenv_MPI_nipype11.sh
+++ environs/setenv_MPI_nipype11.sh
@@ -2,8 +2,10 @@
 
 
 PYTHONPATH=${PYTHONPATH}:/home/raid2/liem/Dropbox/Workspace:/home/raid2/liem/Dropbox/Workspace/LeiCA:/home/raid2/liem/Dropbox/Workspace/LeiCA/preprocessing:/home/raid2/liem/Dropbox/Workspace/LeiCA/preprocessing/cpac_0391_local
+PYTHONPATH=/home/raid2/liem/nipype11:$PYTHONPATH
+
 export PYTHONPATH
 
+source /home/raid2/liem/virtualenvs/leica/bin/activate
 
-#make sure that DCMSTACK is called AFTER CPAC
-FSL --version 5.0 FREESURFER --version 5.3.0 AFNI CPAC DCMSTACK PANDAS NILEARN SKLEARN MATPLOTLIB NUMPY SEABORN
+FSL --version 5.0 FREESURFER --version 5.3.0 AFNI ANTSENV MATPLOTLIB CPAC
