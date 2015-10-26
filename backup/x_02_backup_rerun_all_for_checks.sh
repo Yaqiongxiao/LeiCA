5ef52204e0f6d2425db42e11903d93aee4d36f4f
fliem <franz.liem>
2015-10-26 17:19:50 +0100

    fixed and added check

--- backup/x_02_backup_rerun_all_for_checks.sh
+++ backup/x_02_backup_rerun_all_for_checks.sh
@@ -12,7 +12,7 @@ cd $source_path
 
 
 for D in 0*; do
-    cmd="rsync -av --no-o --no-g ${source_path}/${D}/ ${target_path}/${D}/"
+    cmd="rsync -a --no-o --no-g ${source_path}/${D}/ ${target_path}/${D}/"
     $cmd
 done
 
@@ -20,7 +20,7 @@ done
 # because /a cannot take IDs with more than 9 chars, the heading 0 (after the A) is cut
 for D in A*; do
     short_id=A${D:2}
-    cmd="rsync -av --no-o --no-g ${source_path}/${D}/ ${target_path}/${short_id}/"
+    cmd="rsync -a --no-o --no-g ${source_path}/${D}/ ${target_path}/${short_id}/"
     $cmd
 done
 
